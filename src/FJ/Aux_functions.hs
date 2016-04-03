0d575b155c18cd161232a979f23a9139cdb217ab
pedroabreu <abreu223@hotmail.com>
2016-04-03 18:17:55 -0300

    Adding Field lookup Function Please rerun cbnf on FJ, I couldn't make it to ouput on the correct folder, so I made the function under a dummy folder ps: do not run cbnf with -m flag for it will rewrite our makefile

---
+++ src/FJ/Aux_functions.hs
@@ -0,0 +1,27 @@
+module FJ.Meta_functions where
+
+import FJ.AbsFjSyntax
+
+
+get_ct :: Prog -> [CD]
+get_ct (Program ct _) = ct 
+
+transFD_Field :: [FD] -> [Field]
+transFD_Field [] = []
+transFD_Field ((FDecl t i):xs) = (Field t i):transFD_Field xs
+
+field_lookup' :: [CD] -> [CD] -> Type -> [Field]
+field_lookup' _ _ TypeObject = []
+field_lookup' [] _ _ = []
+field_lookup' ct@(CDecl id parent_class fields  _ _ : xs) consumed (TypeId id') = 
+            if id == id' then (transFD_Field fields) ++ (field_lookup (ct++consumed) parent_class)
+            else field_lookup' xs (x:consumed) (TypeId id') 
+  where x = head ct
+
+field_lookup :: [CD] -> Type -> [Field]
+field_lookup ct ty = field_lookup' ct [] ty
+
+test_prog = Program [CDecl (Id "teste") TypeObject [FDecl TypeObject (Id "a")] (KDecl (Id "teste") [Field TypeObject (Id "a")] [] [Assignment (Id "a") (Id "a")]) [], CDecl (Id "teste2") (TypeId $ Id "teste") [] (KDecl (Id "teste2") [] [] []) []] (NewExp (Id "teste") [])
+
+test_progCT = get_ct test_prog
+

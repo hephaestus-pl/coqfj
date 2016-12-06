53c1c57cb8a191096569a42507d797e72b96fa7e
pedrotst <abreu223@hotmail.com>
2016-12-06 16:27:59 -0200

    Move lists lemma to metatheory

--- src/Metatheory.v
+++ src/Metatheory.v
@@ -79,6 +79,21 @@ Proof.
   apply n1; inversion H; auto.
 Qed.
 
+Lemma in_notin_noteq: forall {A:Type} xs (x1: A) x2,
+  In x1 xs ->
+  ~In x2 xs ->
+  x1 <> x2.
+Proof.
+  induction xs.
+  intros; inversion H.
+  intros.
+  simpl in *.
+  apply Decidable.not_or in H0. destruct H0.
+  destruct H.
+  rewrite <- H; auto.
+  apply IHxs; auto.
+Qed.
+
 Fixpoint find_w (n: nat) (key: id) (l: list id) :=
   match l with
     | [] => None
--- src/env.v
+++ src/env.v
@@ -48,21 +48,6 @@ Proof.
 Qed.
 Hint Resolve snoc_app.
 
-Lemma in_notin_noteq: forall {A:Type} xs (x1: A) x2,
-  In x1 xs ->
-  ~In x2 xs ->
-  x1 <> x2.
-Proof.
-  induction xs.
-  intros; inversion H.
-  intros.
-  simpl in *.
-  apply Decidable.not_or in H0. destruct H0.
-  destruct H.
-  rewrite <- H; auto.
-  apply IHxs; auto.
-Qed.
-
 Lemma extend_nshadow': forall {A: Type} (m: env A) x a,
   In x (Dom m) ->
   get (m extd x : a) x = get m x.

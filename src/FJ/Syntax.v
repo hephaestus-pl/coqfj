64e0997840a2f23b5c8e2b1c4522a9dd8351d0ce
pedrotst <abreu223@hotmail.com>
2017-03-16 00:18:25 -0300

    Adds comentary

--- src/FJ/Syntax.v
+++ src/FJ/Syntax.v
@@ -1,43 +1,58 @@
 Require Import String.
 Require Import FJ.Lists.
 Require Import FJ.Base.
-(* We will use Notation to make automation easier
- * This will be the notation to be similar with haskell *)
+(* This will be the notation to be similar with haskell *)
 Notation "'[' X ']'" := (list X) (at level 40).
 
+(* We could use Inductive for ClassNames and Vars, 
+ * but would make the other definitions cumbersome to deal with 
+ * the special names Object and this.
+ * ClassNames are our types.
+ *)
 Definition ClassName := id.
 Parameter Object: ClassName.
 
+(* Vars must appear only inside methods body *)
 Definition Var := id.
 Parameter this: Var.
 
 Inductive Argument :=
   | Arg : id -> Argument.
 
+(* FormalArg and FieldDecl is a ClassName (i.e. a type) and an id *)
 Inductive FormalArg :=
   | FArg : ClassName -> id -> FormalArg.
+Inductive FieldDecl :=
+  | FDecl : ClassName -> id -> FieldDecl.
 
+(* Referable essentialy means I can use the ref function
+ * to retrieve the id of a value.
+ * And it also gives me a nice function ´find´ for free.
+ * See Util.Referable
+*)
 Instance FargRef : Referable FormalArg :={
   ref farg := 
     match farg with 
    | FArg _ id => id end
 }.
-
-Definition fargType (f: FormalArg):ClassName := 
-  match f with FArg t _ => t end.
-
-Inductive FieldDecl :=
-  | FDecl : ClassName -> id -> FieldDecl.
-
 Instance FieldRef : Referable FieldDecl :={
   ref fdecl := 
     match fdecl with 
    | FDecl _ id => id end
 }.
 
+(* fargType and fieldType are a means to retrieve the Type of the declarations *)
+Definition fargType (f: FormalArg):ClassName := 
+  match f with FArg t _ => t end.
 Definition fieldType (f: FieldDecl): ClassName := 
   match f with FDecl t _ => t end.
 
+(* Our expressions are Variables,
+ * field acesses,
+ * method invocations,
+ * cast
+ * and new
+*)
 Inductive Exp : Type :=
   | ExpVar : Var -> Exp
   | ExpFieldAccess : Exp -> id -> Exp

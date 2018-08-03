6bb9315cf2f9bbdb7a1cf16d6f9b7a629e5c1499
Pedro Abreu <abreu223@hotmail.com>
2018-08-02 21:56:44 -0400

    Better Ids

--- src/Util/Id.v
+++ src/Util/Id.v
@@ -1,58 +1,102 @@
+Require Import Arith NPeano.
+Require Import Bool.
 
-Require Import Arith.
-Require Import NPeano.
+Require Import DecidableClass.
 
-(* Identifiers and polymorphic partial maps. *)
-Inductive id : Type := 
-  Id : nat -> id.
-
-Definition beq_id id1 id2 :=
-  match (id1, id2) with
-    (Id n1, Id n2) => beq_nat n1 n2
-  end.
-
-Theorem beq_id_refl : forall i,
-  beq_id i i = true.
+Lemma reflectF_iff: forall P b, (reflect P b) -> (~P <-> b = false).
 Proof.
-  intros. destruct i. symmetry.
-  apply beq_nat_refl.  Qed.
+  induction 1; intuition.
+Qed.
 
-Theorem beq_id_eq : forall i1 i2,
-  beq_id i1 i2 = true -> i1 = i2.
-Proof.
-  intros i1 i2 H.
-  destruct i1. destruct i2. symmetry in H.
-  apply beq_nat_eq in H. subst.
-  reflexivity.  Qed.
+Section Id.
+  Inductive id : Type := 
+    Id : nat -> id.
 
-Theorem beq_id_false_neq : forall i1 i2,
-  beq_id i1 i2 = false -> i1 <> i2.
-Proof.
-  intros i1 i2 H.
-  destruct i1. destruct i2.
-  apply beq_nat_false in H.
-  intros C. apply H. inversion C. reflexivity.  Qed.
+  Definition id_nat (n: id):= match n with Id x => x end.
+  Definition eqb i1 i2 := Nat.eqb (id_nat i1) (id_nat i2).
+  Infix "=?" := eqb.
 
-Theorem not_eq_beq_id_false : forall i1 i2,
-  i1 <> i2 -> beq_id i1 i2 = false.
-Proof.
-  intros i1 i2 H.
-  destruct i1. destruct i2.
-  assert (n <> n0).
-    intros C. subst. apply H. reflexivity.
-  apply beq_nat_false_iff. assumption.  Qed.
+  Lemma eqb_nat: forall (x y: nat), (Id x) = (Id y) <-> x = y.
+  Proof.
+    intros.
+    split; intro H; try inversion H; auto.
+  Qed.
+
+  Lemma eqb_spec i1 i2 : reflect (i1 = i2) (i1 =? i2).
+  Proof.
+    destruct i1,i2.
+    decide (n = n0); subst; simpl; unfold eqb; simpl.
+    * rewrite Nat.eqb_refl.
+      apply ReflectT; auto.
+    * pose proof (Nat.eqb_neq n n0) as sth.
+      destruct sth.
+      rewrite H1; auto.
+      apply ReflectF; auto.
+      congruence.
+  Qed.
+
+  Instance id_eqdec : forall (i1 i2: id), Decidable (eq i1 i2) :=
+    { Decidable_witness := eqb i1 i2}.
+  Proof.
+    pose proof (reflect_iff) as sth.
+    symmetry.
+    apply sth.
+    apply eqb_spec.
+  Qed.
+
+  
+  Theorem beq_id_refl : forall i,
+      i =? i = true.
+  Proof.
+    intros.
+    destruct i.
+    apply (reflect_iff _ _ (eqb_spec _ _)).
+    reflexivity.
+  Qed.
+
+  Theorem beq_id_eq : forall i1 i2,
+      i1 =? i2 = true -> i1 = i2.
+  Proof.
+    intros i1 i2 H.
+    apply (reflect_iff _ _ (eqb_spec _ _)).
+    assumption.
+  Qed.
+
+  Theorem beq_id_false_neq : forall i1 i2,
+      i1 =? i2 = false -> i1 <> i2.
+  Proof.
+    intros i1 i2 H.
+    destruct i1, i2.
+    apply beq_nat_false in H.
+    intros C.
+    inversion C; congruence.
+  Qed.
+
+  Theorem not_eq_beq_id_false : forall i1 i2,
+      i1 <> i2 -> i1 =? i2 = false.
+  Proof.
+    intros i1 i2 H.
+    destruct i1, i2.
+    assert (n <> n0) by intuition.
+    apply beq_nat_false_iff. assumption.
+  Qed.
+
+
+
+  Theorem beq_id_sym: forall i1 i2,
+      (i1 =? i2) = (i2 =? i1).
+  Proof.
+    intros i1 i2.
+    destruct i1, i2.
+    apply Nat.eqb_sym.
+  Qed.
+
+  Theorem beq_id_dec: forall (i1 i2: id),
+      {i1 = i2} + {i1 <> i2}.
+  Proof. 
+    repeat decide equality.
+  Defined.
+Section Id.
 
 Hint Resolve beq_id_false_neq.
 Hint Rewrite beq_id_refl.
-
-
-Theorem beq_id_sym: forall i1 i2,
-  beq_id i1 i2 = beq_id i2 i1.
-Proof.
-  intros i1 i2. destruct i1. destruct i2. apply NPeano.Nat.eqb_sym. Qed.
-
-Theorem beq_id_dec: forall (i1 i2: id),
-  {i1 = i2} + {i1 <> i2}.
-Proof. 
-  repeat decide equality.
-Defined.
